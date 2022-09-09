74460dd600b75b3eeb48bec728046885f50a0775
Yohanes-GR <yohgut@gmail.com>
2022-09-10 00:51:29 +0300

    update scripts

--- scripts/logger_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-LOGGING_FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
-
-logging.basicConfig(filename='../logs/logs.txt',
-                    level=logging.DEBUG,
-                    format=LOGGING_FORMAT)
-
-logger = logging.getLogger()
