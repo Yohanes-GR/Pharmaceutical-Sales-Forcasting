e9cb211f15810e864861659e4876d351a94b62e2
Yohanes Gutema <99284608+Yohanes-GR@users.noreply.github.com>
2022-09-10 22:07:57 +0300

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,1 +1,13 @@
+import user_overview_page
+import promotion_page
 
+import streamlit as st
+
+PAGES = {
+     "Data Overview": user_overview_page,
+     "Promotion Effect on Stores": promotion_page
+}
+
+selection = st.sidebar.radio("Go to page", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
